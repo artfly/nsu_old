5b485a0e99aaeddc451e8ea1bb4b95303553a9ad
artfly <artfly94@gmail.com>
2014-11-10 11:44:33 +0300

    Added time functions

--- hope.cpp
+++ hope.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <X11/Xlib.h>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <time.h>
 
 #define SPACE 32
 #define ESC 27
@@ -14,15 +15,25 @@ int help (std::string pr_name) {
 	return 1;
 }
 
+
+
 int main (int argc, char **argv) {
 	if (argc != 2) {
 		return help (std::string(argv[0]));
 	}
 	char *imageName = argv[1];
+	struct timespec start, end;
 	
 	cv::Mat image;
-	cv::VideoCapture capture(0);
 	cv::Mat hope = cv::imread ("hopeless.png");
+
+	clock_gettime (CLOCK_MONOTONIC_RAW, &start);
+	cv::VideoCapture capture(0);
+	clock_gettime (CLOCK_MONOTONIC_RAW, &end);
+	std::cout <<  "Time taken to get capture : " 
+						<< end.tv_sec-start.tv_sec + 0.000000001* (end.tv_nsec-start.tv_nsec)
+						<< " sec \n";
+	
 	cv::Mat hope_resize;
 	cv::Mat result;
 	
@@ -41,13 +52,26 @@ int main (int argc, char **argv) {
 		cv::moveWindow ("Camera", (scrn->width-W_WIDTH)/2, (scrn->height-W_HEIGHT)/2);
 		char c = cv::waitKey (33);
 		if (c == SPACE) {
+
+			clock_gettime (CLOCK_MONOTONIC_RAW, &start);
 			cv::cvtColor (frame, image, CV_BGR2GRAY);
 			cv::imwrite (imageName, image);
 			image  = cv::imread (imageName);
 			cv::resize (hope, hope_resize, cv::Size2i (image.cols, image.rows));
 			cv::addWeighted (image, 1, hope_resize, 0.5, 0.0, result);
 			cv::imwrite (imageName, result);
+			clock_gettime (CLOCK_MONOTONIC_RAW, &end);
+			std::cout <<  "Time taken to transform image : " 
+							<< end.tv_sec-start.tv_sec + 0.000000001* (end.tv_nsec-start.tv_nsec)
+							<< " sec \n";
+
+			clock_gettime (CLOCK_MONOTONIC_RAW, &start);
 			cv::imshow ("Camera", result);
+			clock_gettime (CLOCK_MONOTONIC_RAW, &end);
+			std::cout <<  "Time taken to show image : " 
+							<< end.tv_sec-start.tv_sec + 0.000000001* (end.tv_nsec-start.tv_nsec)
+							<< " sec \n";
+
 			cv::waitKey (2500);
 		}	
 		if (c == ESC) {
